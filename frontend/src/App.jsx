7539d4238a93a1f3414dbd514b0a4d6f2beddfc5
Ihsan <ihsanahammed908@gmail.com>
2025-03-03 17:13:08 +0530

    added assets and navbar component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar/Navbar'
 
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <div className='app'>
+      <Navbar />  
+    </div>
   )
 }
 
