dbfc40bb2667012c7183846021bd5ddc8fae6c47
Ihsan <ihsanahammed908@gmail.com>
2025-03-04 14:19:45 +0530

    added header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
 
 
 function App() {
@@ -8,6 +10,9 @@ function App() {
   return (
     <div className='app'>
       <Navbar />  
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
     </div>
   )
 }
