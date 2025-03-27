c8c6d679579977bf8b0f8b8af956ef0ee1894aeb
Ihsan <ihsanahammed908@gmail.com>
2025-03-27 15:04:12 +0530

    pages folder moved to outside the components folder

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
 import { Routes,Route } from 'react-router-dom'
-import Add from './components/pages/Add/Add'
+import Add from './pages/Add/Add'
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/ReactToastify.css'
-import List from './components/pages/List/List'
-import Order from './components/pages/Orders/Order'
+import List from './pages/List/List'
+import Order from './pages/Orders/Order'
 
 const App = () => {
 
