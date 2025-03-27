15199cd1e35f490b1c179e5fc8af45d2989d0f91
Ihsan <ihsanahammed908@gmail.com>
2025-03-27 17:06:47 +0530

    url changed to render domain in frontend

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({})
-    const url = 'http://localhost:3000'
+    const url = 'https://food-delivery-2-ep79.onrender.com'
     const [token, setToken] = useState('')
     const [food_list, setFoodList] = useState([])
 
