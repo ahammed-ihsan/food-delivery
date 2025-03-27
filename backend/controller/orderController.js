368a706360efd04150a81c25e7623fba61758624
Ihsan <ihsanahammed908@gmail.com>
2025-03-27 11:42:56 +0530

    added route for update order status

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -92,4 +92,15 @@ const listOrders = async (req, res) => {
     }
 }
 
-export {placeOrder, verifyOrder, userOrders, listOrders}
+//update order status
+const updateStatus = async (req, res) => {
+    try{
+        await orderModel.findByIdAndUpdate(req.body.orderId,{status: req.body.status})
+        res.json({success: true, message: 'Status Updated'})
+    }catch(err){
+        console.log(err);
+        res.json({success: false, message: 'Error'})
+    }
+}
+
+export {placeOrder, verifyOrder, userOrders, listOrders, updateStatus}
